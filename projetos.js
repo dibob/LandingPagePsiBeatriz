366587aa2beacb1e06917f9b591591203e7c62b6
Felipe <felipersmed@gmail.com>
2025-04-02 17:10:52 -0300

    Modelo de projetos

--- projetos.js
+++ projetos.js
@@ -2,14 +2,22 @@ const projetos = [
     {
         nome: 'Projeto 1',
         descricao: 'Descrição detalhada do Projeto 1.',
-        imagem: 'imagens/projeto1.jpg',
-        link: 'link-do-projeto-1' // Opcional
+    },
+    {
+        nome: 'Projeto 3',
+        descricao: 'Descrição detalhada do Projeto 3.',
     },
     {
         nome: 'Projeto 2',
         descricao: 'Descrição detalhada do Projeto 2.',
-        imagem: 'imagens/projeto2.jpg',
-        link: 'link-do-projeto-2' // Opcional
+    },
+    {
+        nome: 'Projeto 2',
+        descricao: 'Descrição detalhada do Projeto 2.',
+    },
+    {
+        nome: 'Projeto 2',
+        descricao: 'Descrição detalhada do Projeto 2.',
     },
     // Adicione mais projetos aqui
 ];
